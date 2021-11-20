0c136cf4696ce52676ffef900133306fe622fa32
muhammed yalçın <muhammedyalcin4721@gmail.com>
2021-11-20 10:04:24 +0300

    Sayısal loto ilk versiyon

---
+++ src/com/bilgeadam/sayisalloto/SayisalLoto.java
@@ -0,0 +1,23 @@
+package com.bilgeadam.sayisalloto;
+
+import java.util.Random;
+
+public class SayisalLoto {
+
+    public int sayiGetir( int maxSayi)
+    {
+        Random rnd = new Random();
+       return rnd.nextInt(maxSayi);
+
+    }
+
+    public static void main(String[] args) {
+        SayisalLoto loto = new SayisalLoto();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(loto.sayiGetir(49));
+
+        }
+
+    }
+}
