d8eb8edc63584ff93b28af6827d400a46a2e547c
muhammed yalçın <muhammedyalcin4721@gmail.com>
2021-11-20 12:40:04 +0300

    çoklusartOrnek değişti

--- src/com/bilgeadam/sayisalloto/CokluSartOrnek.java
+++ src/com/bilgeadam/sayisalloto/CokluSartOrnek.java
@@ -7,12 +7,20 @@ public class CokluSartOrnek {
         int sayi1 = 23;
         int sayi2 = 35;
 
-        int sayi3 = 23;
+        int sayi3 = 41;
 
         // veya(or) operatörlüğü ||
+        // ve(and) operatörü &&
         if (sayi3==sayi1 || sayi3==sayi2)
         {
             System.out.println("Tekrarlanan sayı var!");
+        }
+
+        while (sayi3 != sayi1 && sayi3!=sayi2)
+        {
+
+            System.out.println("sayi3 diğerlerine eşit değildir!");
+            sayi3 = sayi1;
         }
 
 
